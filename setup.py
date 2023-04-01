cdcf1b05e163965d8794a62047781c8bf6d45585
Naon <66204230+vNaonLu@users.noreply.github.com>
2023-04-01 22:27:46 +0800

    uploads to pypi. (#2)

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 from src.dcli.version import VERSION
+from pathlib import Path
 
 PACKAGES = find_packages('src')
 
+readme = ""
+with Path("README.md").open("r", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
-    name="dcli",
+    name="decorator-cli",
     version=VERSION,
     license="MIT",
     author="Naon Lu",
@@ -14,6 +19,15 @@ setup(
     url="https://github.com/vNaonLu/dcli",
     keywords="decorator cli",
     description="functional-oriented cli decorator.",
-    long_description="functional-oriented command line interface decorator module.",
-    python_requires=">=3.9"
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 )
